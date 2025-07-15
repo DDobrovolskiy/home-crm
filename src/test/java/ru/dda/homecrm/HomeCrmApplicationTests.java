8d3a0c9977acc582a4ecff3fd5d5d6aa3b2ce1d9
DDobrovolskiy <dda.90@mail.ru>
2025-07-15 19:03:34 +0300

    init

---
+++ src/test/java/ru/dda/homecrm/HomeCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dda.homecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
